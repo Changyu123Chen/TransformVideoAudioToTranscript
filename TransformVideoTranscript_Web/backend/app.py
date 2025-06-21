882f963d37873a7f396633da877700bea9595a7f
CHANGYU CHEN <c536chen@uwaterloo.ca>
2025-06-20 23:02:00 -0400

    changed the logic of upload file, cannot process videos due to memory constraint

--- TransformVideoTranscript_Web/backend/app.py
+++ TransformVideoTranscript_Web/backend/app.py
@@ -18,34 +18,34 @@ def home():
 
 @app.route("/upload", methods=["POST"])
 def upload_files():
-    print(">>> Received upload request")
-    if "files" not in request.files:
-        return jsonify({"error": "No file part"}), 400
+    # print(">>> Received upload request")
+    # if "files" not in request.files:
+    #     return jsonify({"error": "No file part"}), 400
     
-    files = request.files.getlist("files")
-    if not files or files[0].filename == "":
-        return jsonify({"error": "No selected files"}), 400
-    results = []
+    # files = request.files.getlist("files")
+    # if not files or files[0].filename == "":
+    #     return jsonify({"error": "No selected files"}), 400
+    # results = []
 
-    for file in files:
-        filename = file.filename
-        save_path = os.path.join(UPLOAD_FOLDER, filename)
-        file.save(save_path)
+    # for file in files:
+    #     filename = file.filename
+    #     save_path = os.path.join(UPLOAD_FOLDER, filename)
+    #     file.save(save_path)
 
-        audio_path = extract_audio(save_path)
-        text = transcribe_audio(audio_path)
+    #     audio_path = extract_audio(save_path)
+    #     text = transcribe_audio(audio_path)
 
-        transcript_name = filename.rsplit(".", 1)[0] + ".txt"               #rigth split to get the file name without extension
-        transcript_path = os.path.join(TRANSCRIPT_FOLDER, transcript_name)
-        with open(transcript_path, "w") as f:
-            f.write(text)
-        results.append({
-            "filename": filename,
-            "transcript": text,
-            "download_url": f"/download/{transcript_name}"
-        })
-    return jsonify(results)
-
+    #     transcript_name = filename.rsplit(".", 1)[0] + ".txt"               #rigth split to get the file name without extension
+    #     transcript_path = os.path.join(TRANSCRIPT_FOLDER, transcript_name)
+    #     with open(transcript_path, "w") as f:
+    #         f.write(text)
+    #     results.append({
+    #         "filename": filename,
+    #         "transcript": text,
+    #         "download_url": f"/download/{transcript_name}"
+    #     })
+    # return jsonify(results)
+    return jsonify({"transcript": "Sorry, transcription is disabled on the free tier 😢."})
 @app.route("/download/<name>", methods=["GET"])      #default method: get
 def download_file(name):
     return send_from_directory(TRANSCRIPT_FOLDER, name, as_attachment=True)
